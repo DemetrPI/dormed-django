7f627fc0a4f500a1b8c7cb1d41f6a415da91e509
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-04-13 12:27:34 +0200

    Prepare for deploy

--- dormed/dormed/settings.py
+++ dormed/dormed/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p)h3ozv&z_a#9c$yll67fw-(0o4f^g)p&i&ov=#$0@$h=9y7m5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','localhost', 'localhost:8000']
 #for localhost only
 CORS_ORIGIN_ALLOW_ALL = True
 
--- dormed/dormed/wsgi.py
+++ dormed/dormed/wsgi.py
@@ -10,4 +10,4 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dormed.settings')
-app= get_wsgi_application()
+application = get_wsgi_application()
